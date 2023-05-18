92b16cdc244929755d2c9b4059f33739b3dc6057
Meet Kachhara <meetkachhara@gmail.com>
2023-05-18 16:01:39 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img = qr.make("https://www.linkedin.com/in/meet-kachhara-bab544218?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BveWcHtsCQ3Svvjs0fGvqfA%3D%3D")
+img.save("Meet_LinkedIn.png")
